e695ef0903dd6f6a9cb239b79907aeab91d89415
neonzwe <103013071+neonzwe@users.noreply.github.com>
2022-04-14 09:56:43 +0900

    Add files via upload

---
+++ qqweee/qqweee/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  qqweee
+//
+//  Created by Гость on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var buttonAC: UIButton!
+    @IBOutlet weak var buttonZero: UIButton!    
+    @IBOutlet weak var resultLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
